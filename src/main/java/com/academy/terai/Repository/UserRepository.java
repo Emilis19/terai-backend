62ea7e6fd534c9b8d539c4f929bd7111ffa2122f
tomasbutkevicius <tomas.butkevicius99@gmail.com>
2020-03-24 14:40:16 +0200

    Roles and Accounts entities and implementation/Last Logged in as Date

--- src/main/java/com/academy/terai/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.academy.terai.Repository;
-
-import com.academy.terai.Model.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-
-public interface UserRepository extends MongoRepository <User, Integer> {
-
-}
